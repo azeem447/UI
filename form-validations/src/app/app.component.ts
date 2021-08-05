2955635febf33aebdfde239326b1ac6452c0b377
azeem <azeem.sk447@gmail.com>
2021-08-05 15:59:01 +0530

    form-validation task

---
+++ form-validations/src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// import custom validator to validate that password and confirm password fields match
+import { MustMatch } from './_helpers/must-watch.validator';
+
+@Component({ selector: 'app-root', templateUrl: 'app.component.html', styleUrls: ['app.component.css'] })
+export class AppComponent implements OnInit {
+  registerForm: any;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group(
+      {
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        phone: [
+          '',
+          [Validators.required, Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')]
+        ],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required]
+      },
+      {
+        validator: MustMatch('password', 'confirmPassword')
+      }
+    );
+  }
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    // display form values on success
+    console.log(JSON.stringify(this.registerForm.value, null, 4));
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.registerForm.reset();
+  }
+}
